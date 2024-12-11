4567d3036d7bb0d680dbce61cffab1ffaa768b3d
Pavel Yatsevich <mu11er100500@gmail.com>
2024-12-12 00:30:51 +0300

    add parameters to DBConfig class

--- src/main/java/com/ifortex/bookservice/repo/DBConfig.java
+++ src/main/java/com/ifortex/bookservice/repo/DBConfig.java
@@ -1,4 +1,15 @@
 package com.ifortex.bookservice.repo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class DBConfig {
+    @Value("spring.datasource.url")
+    private String url;
+    @Value("spring.datasource.username")
+    private String username;
+    @Value("spring.datasource.password")
+    private String password;
+
+    public DBConfig() {
+    }
 }
