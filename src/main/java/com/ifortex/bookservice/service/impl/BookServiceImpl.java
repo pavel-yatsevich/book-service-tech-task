0a82d373fd17c18531d8a8b38a1eefee8df41007
Pavel Yatsevich <mu11er100500@gmail.com>
2024-12-13 20:47:32 +0300

    refactor BookServiceImpl class

--- src/main/java/com/ifortex/bookservice/service/impl/BookServiceImpl.java
+++ src/main/java/com/ifortex/bookservice/service/impl/BookServiceImpl.java
@@ -2,24 +2,32 @@ package com.ifortex.bookservice.service.impl;
 
 import com.ifortex.bookservice.dto.SearchCriteria;
 import com.ifortex.bookservice.model.Book;
+import com.ifortex.bookservice.repo.BookServiceRepository;
 import com.ifortex.bookservice.service.BookService;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
 public class BookServiceImpl implements BookService {
 
+    private final BookServiceRepository bookServiceRepository = new BookServiceRepository();
 
     @Override
     public Map<String, Long> getBooks() {
-        try (){
-
+        try {
+            return bookServiceRepository.getBooks();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-        return Map.of();
     }
 
     @Override
     public List<Book> getAllByCriteria(SearchCriteria searchCriteria) {
-        return List.of();
+        try {
+            return bookServiceRepository.getAllByCriteria(searchCriteria);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
