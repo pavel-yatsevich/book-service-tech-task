c46d2a2c34839593bc324308dad348c14ac1c17a
Pavel Yatsevich <mu11er100500@gmail.com>
2024-12-14 02:53:25 +0300

    refactor DBConfig classes

--- src/main/java/com/ifortex/bookservice/repo/DBConfig.java
+++ src/main/java/com/ifortex/bookservice/repo/DBConfig.java
@@ -1,14 +1,16 @@
 package com.ifortex.bookservice.repo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DBConfig {
-    @Value("spring.datasource.url")
-    protected String url;
-    @Value("spring.datasource.username")
-    protected String username;
-    @Value("spring.datasource.password")
-    protected String password;
+    @Value("${spring.datasource.url}")
+    protected String url ;
+    @Value("${spring.datasource.username}")
+    protected String username ;
+    @Value("${spring.datasource.password}")
+    protected String password ;
 
     public DBConfig() {
     }
