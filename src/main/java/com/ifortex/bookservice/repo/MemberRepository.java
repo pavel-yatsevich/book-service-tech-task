700a8282c942cac7f3f59174957ef13f805c2790
Pavel Yatsevich <mu11er100500@gmail.com>
2024-12-14 13:45:34 +0300

    add findMembers() method to MemberRepository class

--- src/main/java/com/ifortex/bookservice/repo/MemberRepository.java
+++ src/main/java/com/ifortex/bookservice/repo/MemberRepository.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MemberRepository extends DBConfig {
@@ -15,7 +16,7 @@ public class MemberRepository extends DBConfig {
         String getMemberSQL = "SELECT member_id AS id, name, membership_date FROM member_books " +
                 "JOIN books b ON b.id = book_id JOIN members m ON m.id = member_id " +
                 "WHERE array_to_string(genre, ',') LIKE '%Romance' " +
-                "ORDER BY publication_date, membership_date DESC LIMIT 1;";
+                "ORDER BY publication_date, membership_date DESC LIMIT 1";
         Member member = new Member();
         
         try (Connection connection = DriverManager.getConnection(url, username, password);
@@ -31,6 +32,22 @@ public class MemberRepository extends DBConfig {
     }
 
     public List<Member> findMembers() throws SQLException {
-        return null;
+        String getMemberSQL = "select * from members " +
+                "where membership_date between '2023-01-01 00:00:00.000000' and '2023-12-31 23:59:59.999999' " +
+                "and not exists(select member_books.member_id from member_books where member_id = members.id)";
+        List<Member> membersList = new ArrayList<>();
+
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+             ResultSet resultSet = connection.prepareStatement(getMemberSQL)
+                     .executeQuery()) {
+            while (resultSet.next()) {
+                Member member = new Member();
+                member.setId(resultSet.getInt("id"));
+                member.setName(resultSet.getString("name"));
+                member.setMembershipDate(resultSet.getTimestamp("membership_date").toLocalDateTime());
+                membersList.add(member);
+            }
+        }
+        return membersList;
     }
 }
