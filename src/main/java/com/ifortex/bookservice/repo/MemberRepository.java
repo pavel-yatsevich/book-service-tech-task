b230d9ce58d410a0c9ae4bdbc4c7d04e540259b6
Pavel Yatsevich <mu11er100500@gmail.com>
2024-12-14 13:38:38 +0300

    add findMember() method to MemberRepository class

--- src/main/java/com/ifortex/bookservice/repo/MemberRepository.java
+++ src/main/java/com/ifortex/bookservice/repo/MemberRepository.java
@@ -1,4 +1,36 @@
 package com.ifortex.bookservice.repo;
 
+import com.ifortex.bookservice.model.Book;
+import com.ifortex.bookservice.model.Member;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 public class MemberRepository extends DBConfig {
+
+    public Member findMember() throws SQLException {
+        String getMemberSQL = "SELECT member_id AS id, name, membership_date FROM member_books " +
+                "JOIN books b ON b.id = book_id JOIN members m ON m.id = member_id " +
+                "WHERE array_to_string(genre, ',') LIKE '%Romance' " +
+                "ORDER BY publication_date, membership_date DESC LIMIT 1;";
+        Member member = new Member();
+        
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+             ResultSet resultSet = connection.prepareStatement(getMemberSQL)
+                     .executeQuery()) {
+            while (resultSet.next()) {
+                member.setId(resultSet.getInt("id"));
+                member.setName(resultSet.getString("name"));
+                member.setMembershipDate(resultSet.getTimestamp("membership_date").toLocalDateTime());
+            }
+        }
+        return member;
+    }
+
+    public List<Member> findMembers() throws SQLException {
+        return null;
+    }
 }
