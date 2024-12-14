f1c12c051d0ea52e41ed311169a15077757180bd
Pavel Yatsevich <mu11er100500@gmail.com>
2024-12-14 13:46:46 +0300

    add MemberServiceImpl class

---
+++ src/main/java/com/ifortex/bookservice/service/impl/MemberServiceImpl.java
@@ -0,0 +1,18 @@
+package com.ifortex.bookservice.service.impl;
+
+import com.ifortex.bookservice.model.Member;
+import com.ifortex.bookservice.service.MemberService;
+
+import java.util.List;
+
+public class MemberServiceImpl implements MemberService {
+    @Override
+    public Member findMember() {
+        return null;
+    }
+
+    @Override
+    public List<Member> findMembers() {
+        return List.of();
+    }
+}
